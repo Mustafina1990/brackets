3555fd2bbd77295a34b9788ad4befa11be6ba0f4
Yuliia Mustafina <mystafinayulija@gmail.com>
2021-03-12 17:16:49 +0200

    feat: add function solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let current = 0;
+  let deleteBr = () => {
+    current = str.length;
+    bracketsConfig.forEach(element => str = str.replace(element.join(""), ""));
+    if (current !== str.length) deleteBr();
+  };
+  deleteBr();
+  return !str;
 }
